2282ef11a18062096880315d8fc896cc5a39e3ea
fil <filipisee@hotmail.com>
2022-09-02 15:06:53 -0300

    structuring form login

--- views/Login.vue
+++ views/Login.vue
@@ -1,9 +1,26 @@
 <template>
-  <div>Login</div>
+  <main>
+    <header>
+      <h1 class="logo">FORM FULLSTACK</h1>
+      <h2>Login</h2>
+      <p>Faça o login ou crie uma nova conta.</p>
+    </header>
+
+    <form @submit.prevent="">
+      <label>
+        <span>Seu Email</span>
+        <input type="email" v-model="email" placeholder="Digite o email">
+      </label>
+    </form>
+  </main>
 </template>
   
 <script setup>
-  
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
 </script>
   
 <style scoped>
