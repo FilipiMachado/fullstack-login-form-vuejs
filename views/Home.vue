783314baf879dd835642828aa9c59cb70f5e5b39
fil <filipisee@hotmail.com>
2022-09-03 14:35:36 -0300

    finished frontend strcuture

--- views/Home.vue
+++ views/Home.vue
@@ -1,11 +1,16 @@
 <template>
-  <div>Home</div>
+  <main>
+    <h1>Hello World!</h1>
+    <button>Logout</button>
+  </main>
 </template>
 
-<script setup>
-
-</script>
-
 <style scoped>
+main {
+  padding: 1.5rem;
+}
 
+h1 {
+  margin-bottom: 1rem;
+}
 </style>
