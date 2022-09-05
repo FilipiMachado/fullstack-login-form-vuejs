983cbe3c14e492b6cb449d0ecb2318c81940f7c4
fil <filipisee@hotmail.com>
2022-09-05 11:19:55 -0300

    working on post request

--- server/server.js
+++ server/server.js
@@ -6,3 +6,40 @@ import cors from 'cors'
 dotenv.config()
 
 const app = express()
+
+const client = new Client({
+    project_id: process.env.PROJECT_ID,
+    secret: process.env.SECRET_KEY,
+    env: envs.test
+});
+
+const port = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', async (req, res) => {
+    const { email, password} = req.body;
+
+    try {
+        const resp = await client.passwords.create({
+            email,
+            password,
+            session_duration_minutes: 60,
+        })
+
+        res.json({
+            success: true,
+            message: "User created successfully",
+            token: resp.session_token,
+        })
+    } catch (err) {
+        console.log(err)
+
+        res.json({
+            success: false,
+            message: err.error_message,
+            err: err,
+        })
+    }
+})
