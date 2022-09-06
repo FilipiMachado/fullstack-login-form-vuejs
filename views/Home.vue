b20487f9fdba4de7c2c80f12db303b3631a04afa
fil <filipisee@hotmail.com>
2022-09-05 21:02:25 -0300

    working on logout function

--- views/Home.vue
+++ views/Home.vue
@@ -1,9 +1,33 @@
 <template>
   <main>
     <h1>Hello World!</h1>
-    <button>Logout</button>
+    <button @click="Logout">Logout</button>
   </main>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router'
+
+  const router = useRouter()
+  const Logout = async () => {
+    const res = await fetch('http://localhost:3333/logout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        session_token: localStorage.getItem('token')
+      })
+    }).then(res => res.json())
+
+    if (res.success) {
+      localStorage.removeItem('token')
+      router.push('/login')
+    } else {
+      alert(res.message)
+    }
+  }
+</script>
 
 <style scoped>
 main {
